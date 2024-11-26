69d7ae1cb793a7555e2458b6d085bb622fa70ae2
inacio <inaciogs03@gmail.com>
2024-11-26 17:09:24 -0300

    [windowsforms] - manipulando formulários

---
+++ WindowsForms/WindowsFormsApp/WindowsFormsApp/Form1.Designer.cs
@@ -0,0 +1,98 @@
+﻿
+namespace WindowsFormsApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblNomeUsuario = new System.Windows.Forms.Label();
+            this.txtNomeUsuario = new System.Windows.Forms.TextBox();
+            this.btnClick = new System.Windows.Forms.Button();
+            this.btnShow = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblNomeUsuario
+            // 
+            this.lblNomeUsuario.AutoSize = true;
+            this.lblNomeUsuario.Location = new System.Drawing.Point(45, 64);
+            this.lblNomeUsuario.Name = "lblNomeUsuario";
+            this.lblNomeUsuario.Size = new System.Drawing.Size(135, 20);
+            this.lblNomeUsuario.TabIndex = 0;
+            this.lblNomeUsuario.Text = "Nome Do Usuário";
+            // 
+            // txtNomeUsuario
+            // 
+            this.txtNomeUsuario.Location = new System.Drawing.Point(49, 87);
+            this.txtNomeUsuario.Name = "txtNomeUsuario";
+            this.txtNomeUsuario.Size = new System.Drawing.Size(263, 26);
+            this.txtNomeUsuario.TabIndex = 1;
+            // 
+            // btnClick
+            // 
+            this.btnClick.Location = new System.Drawing.Point(49, 120);
+            this.btnClick.Name = "btnClick";
+            this.btnClick.Size = new System.Drawing.Size(263, 77);
+            this.btnClick.TabIndex = 2;
+            this.btnClick.Text = "Clicar";
+            this.btnClick.UseVisualStyleBackColor = true;
+            this.btnClick.Click += new System.EventHandler(this.btnClick_Click);
+            // 
+            // btnShow
+            // 
+            this.btnShow.Location = new System.Drawing.Point(49, 204);
+            this.btnShow.Name = "btnShow";
+            this.btnShow.Size = new System.Drawing.Size(263, 107);
+            this.btnShow.TabIndex = 3;
+            this.btnShow.Text = "SHOW Formulário";
+            this.btnShow.UseVisualStyleBackColor = true;
+            this.btnShow.Click += new System.EventHandler(this.btnShow_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnShow);
+            this.Controls.Add(this.btnClick);
+            this.Controls.Add(this.txtNomeUsuario);
+            this.Controls.Add(this.lblNomeUsuario);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblNomeUsuario;
+        private System.Windows.Forms.TextBox txtNomeUsuario;
+        private System.Windows.Forms.Button btnClick;
+        private System.Windows.Forms.Button btnShow;
+    }
+}
+
